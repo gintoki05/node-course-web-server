9045b96141a5484d627b19f4a1bcd2ed25783f33
gintoki05 <ajieprastyo@gmail.com>
2018-11-04 08:22:59 +0700

    Repair

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = process.env.PORT || 3000;
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -11,8 +10,8 @@ app.set('view engine', 'hbs');
 app.use((req, res, next) => {
   const now = new Date().toString();
   const log = `${now}: ${req.method} ${req.url}`;
-  console.log(now);
 
+  console.log(log);
   fs.appendFile('server.log', log + '\n', err => {
     if (err) {
       console.log('Unable to append');
@@ -21,7 +20,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// Maintenance
 // app.use((req, res, next) => {
 //   res.render('maintenance.hbs');
 // });
@@ -32,31 +30,21 @@ hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
 
+hbs.registerHelper('screamIt', text => {
+  return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
   res.render('home.hbs', {
-    pageTitle: 'Home',
-    welcomeMessage: 'Ahlan'
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Assalamualaikum Home'
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About'
+    pageTitle: 'About Page'
   });
 });
 
-app.get('/project', (req, res) => {
-  res.render('project.hbs', {
-    pageTitle: 'Project'
-  });
-});
-
-app.get('/bad', (req, res) => {
-  res.send({
-    errorMessage: 'Unable handle request'
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Server up with port ${port}`);
-});
+app.listen(3000);
